396b1508825998833e09e5a28c4fedf6605da700
kurtbartholomew <siever246@gmail.com>
2015-04-03 16:52:27 -0700

    Added drag behavior!

--- watchout.js
+++ watchout.js
@@ -6,12 +6,28 @@ var randomNum = function(dimension){
   return Math.floor(Math.random() * dimension);
 }
 
+// var followMouse = function(){
+//   var coords = d3.mouse(this);
+//   user.attr('cx',coords[0])
+//       .attr('cy',coords[1]);
+// }
+
 var update = function(){
   svg.selectAll('.enemy')
      .transition()
      .duration(1500)
      .attr('cx', function(d){ return randomNum(width-20) })
      .attr('cy', function(d){ return randomNum(height-20) });
+}
+
+var drag = d3.behavior.drag()
+    .on("drag", dragmove);
+
+function dragmove(d) {
+  var x = d3.event.x;
+  var y = d3.event.y;
+  d3.select('#player').attr("cx", x)
+                      .attr("cy", y);
 }
 
 var svg = d3.select('body')
@@ -37,11 +53,14 @@ circles.attr('cx', function(d){ return randomNum(width-20) })
 user.attr('cx', width/2)
     .attr('cy', height/2)
     .attr('r', 10)
-    .attr('class', 'player')
+    .attr('id', 'player')
     .style('fill', 'yellow')
     .style('stroke', 'black')
-    .style('stroke-width', 7);
+    .style('stroke-width', 7)
+    .call(drag);
+    //.on('mousedown', followMouse);
 
 setInterval(update , 1000);
+
 
 
